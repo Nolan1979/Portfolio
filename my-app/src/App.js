d826a06fe4688c33c0cb1d86f0c6473033904fc4
Josh Nolan <nolanjosh1979@gmail.com>
2021-10-12 17:59:34 -0500

    added some styling and navbar components

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <PortfolioContainer />
+      </header>
+      <img className="port" alt="" />
+    </div>
+  );
+    
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+// const Header = styled.div`
+// { 
+//     display: flex;
+//     justify-content: space-between;
+//     text-align: center;
+//     color:rgb(174, 203, 230);
+//     background-color: #0e1820d5;
+//     border-radius: 24px 24px 24px 24px;
+//     text-shadow: 0px 5px 6px rgba(0, 0, 0, 0.678);
+//     height: 100px;
+//     padding: 25px;
+//     margin: 20px;
+//     box-shadow: 0px 7px 10px rgba(3, 3, 3, 0.68);
+// }
+
+// `;
+
+// export default Header
