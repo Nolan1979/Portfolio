a627a46fe1a7695a5e14d58770ccd93d972f74dd
Josh Nolan <nolanjosh1979@gmail.com>
2021-11-03 12:59:34 -0500

    added alt tag to header img

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <div className="pt-32 lg:flex items-center relative z-10 container mx-auto">
             <div className="w-full lg:w-1/2 h-full lg:pr-10 xl:pr-0">
-                <img className="mx-auto rounded-3xl" src="https://res.cloudinary.com/cosmic-records/image/upload/v1635786720/cosmic%20records/port_image_chicago_iirrur.jpg" />
+                <img className="mx-auto rounded-3xl" src="https://res.cloudinary.com/cosmic-records/image/upload/v1635786720/cosmic%20records/port_image_chicago_iirrur.jpg" alt="" />
             </div>
             <div className="w-full lg:w-1/2 h-full">
                 <p className="text-blue font-regular text-4xl md:text-7xl">Josh Nolan</p>
